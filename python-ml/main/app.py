684cd8b7ee363ab22771d3003ffc2015a105803d
JOTHIR ADITHYA <tkjoadi2004@gmail.com>
2026-02-08 13:33:09 +0530

    Update app.py

--- python-ml/main/app.py
+++ python-ml/main/app.py
@@ -49,5 +49,6 @@ def ask():
 
 
 if __name__ == "__main__":
-    app.run(port=5001, debug=True)
+    port = int(os.environ.get("PORT", 10000))  # fallback = 10000
+    app.run(host="0.0.0.0", port=port, debug=False)
 
