f9f6e0f318a0179b4dc34bb616d2ef56222f806d
JOTHIR ADITHYA <tkjoadi2004@gmail.com>
2026-02-08 13:40:08 +0530

    Change API endpoint to production URL

--- frontend/src/components/DigiAsst.jsx
+++ frontend/src/components/DigiAsst.jsx
@@ -21,7 +21,7 @@ export default function DigiAsst() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5001/api/ask", {
+      const res = await fetch("https://digiasst.onrender.com/api/ask", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
